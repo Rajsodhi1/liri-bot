d375b153856f994d4123984fd47b665dcac5fc98
Raj Sodhi <rajsodhiesq@gmail.com>
2019-09-26 23:34:41 -0700

    not working but tried my best to 'complete' it and figure out the issue

--- liri.js
+++ liri.js
@@ -37,13 +37,47 @@ var liri = function (command, query) {
         var table = cTable.getTable(array);
         console.log(table);
     });
+    } else if(command === "movie-this") {
+        var movieName = query;
+        axios.get(queryURL).then(function(response){
+        if (movieName == undefined) {
+            movieName = "Mr. Nobody";
+        }
+        request("http://www.omdbapi.com/?&t=" + query + "&apikey=964d1a21", function (error, response, body) {
+    
+            var result  =  JSON.parse(body);
+            console.log("Title :" + result.Title);
+            console.log("Year :" + result.Released);
+            console.log("IMDB Rating :" + result.imdbRating );
+            console.log("Country :" +  result.Country);
+            console.log("Movie Plot :" + result.Plot);
+            console.log("Actors :" +  result.Actors);
+            })
+        });
+    } else if (command === "concert-this") {
+        var artist = query;
+        var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+        
+        axios.get(queryURL).then(function(response) {
+            console.log(response.data[0]);
+            console.log("Venue name " + response.data[0].venue.name);
+            console.log("Venue location " + response.data[0].venue.city);
+            console.log("Date of Event " +  moment(response.data[0].datetime).format(MM/DD/YYYY));
+            }) 
+            console.log(artist);
+    } else if (command === "do-what-it-says") {
+        fs.readFile("random.txt", "utf8", function (error, data) {
+            if (error) {
+                return console.log("Error: " + error);
+            }
+            var randomTxt = data.split(",");
+            var randomCommand = randomTxt[0];
+            var randomQuery = randomTxt[1];
+            liri(randomCommand, randomQuery);
+        });
+    }
+    };   
 
-
-
-
-
-
-}
 
 
 
